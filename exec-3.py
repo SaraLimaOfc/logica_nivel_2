0bcd2fa5a04cb41c72a3c3e525e25f5f30cb2ee2
SaraLimaOfc <162948693+SaraLimaOfc@users.noreply.github.com>
2024-09-27 19:51:54 -0300

    Explicando o por que alguns valores são diferentes.

---
+++ exec-3.py
@@ -0,0 +1,21 @@
+#A1 
+print((4/2)+(2/4))
+
+#A2
+print((4/2+2/4))
+
+#B1
+print((4/(2+2)/4))
+
+#B2
+print((4/2+2/4))
+
+#C1
+print(((4+2)*2-4))
+
+#C2
+print((4+2*2-4))
+
+# Os valores A1 e A2 possuem o mesmo valor.
+# Os valores B1 e B2 possuem valores diferentes porque em B1, a soma é feita antes da divisão, enquanto em B2 a divisão vem primeiro.
+# Os valores C1 e C2 possuem valores diferentes porque em C1, a soma acontece antes da multiplicação, mas em C2 a multiplicação é priorizada.
