3fe2315e6014285ae39ae79574fee1437636dfee
SaraLimaOfc <162948693+SaraLimaOfc@users.noreply.github.com>
2024-10-02 18:37:12 -0300

    Ler a idade de uma pessoa expressa em anos, meses e dias.

---
+++ exec-7.py
@@ -0,0 +1,12 @@
+anos = int(input("Digite a idade em anos: "))
+
+meses = int(input("Digite a idade em meses: "))
+
+dias = int(input("Digite a idade em dias: "))
+
+anos * 365
+meses * 30
+
+total_dias = anos + meses + dias
+
+print(f"A idade expressa em dias é {total_dias} dias.")
