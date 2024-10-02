9d732260e3238c0a61b22241ad7687504f193f30
SaraLimaOfc <162948693+SaraLimaOfc@users.noreply.github.com>
2024-10-02 19:14:25 -0300

    Cálculo do Custo Final de um Carro Novo

---
+++ exec-10.py
@@ -0,0 +1,12 @@
+custo_fabrica = float(input("Digite o custo de fábrica do carro: "))
+
+percentual_distribuidor = 28  
+percentual_impostos = 45       
+
+custo_distribuidor = custo_fabrica * (percentual_distribuidor / 100)
+custo_impostos = custo_fabrica * (percentual_impostos / 100)
+custo_final = custo_fabrica + custo_distribuidor + custo_impostos
+
+print(f"O custo do distribuidor (28%) é: R${custo_distribuidor:.2f}")
+print(f"O custo dos impostos (45%) é: R${custo_impostos:.2f}")
+print(f"O custo final ao consumidor é: R${custo_final:.2f}")
