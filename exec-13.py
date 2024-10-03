1e9b070d674070809e6bce69ae9258da8e9486e2
SaraLimaOfc <162948693+SaraLimaOfc@users.noreply.github.com>
2024-10-03 20:04:23 -0300

    Ler três notas de um aluno e escrever a média final

---
+++ exec-13.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite a primeira nota:"))
+
+n2 = int(input("Digite a segundaa nota:"))
+
+n3 = int(input("Digite a terceira nota:"))
+
+media_final = (n1 * 2 + n2 * 3 + n3 * 5) / 10
+
+print(f"A nota é: {media_final}.")
