a2013db62e1c37f4da93f1ec8996dfa3d0be5d2b
SaraLimaOfc <162948693+SaraLimaOfc@users.noreply.github.com>
2024-10-04 19:36:31 -0300

    Ler 2 valores e dizer qual o número maior.

---
+++ exec-19.py
@@ -0,0 +1,7 @@
+valor1 = float(input("Digite o primeiro número:"))
+valor2 = float(input("Digite o segundo número:"))
+
+if valor1 > valor2:
+    print(f"O maior valor é: {valor1}")
+else:
+    print(f"O maior valor é: {valor2}")
