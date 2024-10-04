365c3ee94d7bc264c063f179a25965a6aa729051
SaraLimaOfc <162948693+SaraLimaOfc@users.noreply.github.com>
2024-10-04 19:40:49 -0300

    Ler 2 valores e colocar em ordem crescente.

---
+++ exec-20.py
@@ -0,0 +1,7 @@
+valor1 = float(input("Digite o primeiro número:"))
+valor2 = float(input("Digite o segundo número:"))
+
+if valor1 < valor2:
+    print(f"Os valores em ordem crescente são: {valor1}, {valor2}")
+else:
+    print(f"Os valores em ordem crescente são: {valor2}, {valor1}")
