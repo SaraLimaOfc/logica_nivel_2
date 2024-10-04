048c9e91644242b2d88efd2387e55f365b7a715a
SaraLimaOfc <162948693+SaraLimaOfc@users.noreply.github.com>
2024-10-04 19:24:56 -0300

    Ler o ano atual e de nascimento e ver se pode ou não votar.

---
+++ exec-18.py
@@ -0,0 +1,7 @@
+ano_atual = int(input("Digite o ano atual:"))
+ano_nascimento = int(input("Digite o ano do seu nascimento:"))
+
+if ano_nascimento >=18:
+    print("Pode votar")
+else:
+    print("Não pode votar")
